a5394d50ee88d769ba33f914bd55a0172ad8eaa7
pbassdaddy <60669975+pbassdaddy@users.noreply.github.com>
2020-04-29 16:12:08 -0500

    revert
    reverted to playcode test

--- page/script.js
+++ page/script.js
@@ -1,14 +1,16 @@
-(function(){
+$(document).ready(function(){
 
     var lines = 6;
-    
+   
+
     $('#countMeOne').keydown(function(e) {
 
         newLines = $(this).val().split("\n").length;
-        
+       
+
         if(e.keyCode == 13 && newLines >= lines) {
-            
             return false;
         }
+        
     });
 });
