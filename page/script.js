6f6e8ffd8ea09a8d55f073e6ea147c3ad313e429
pbassdaddy <jamiefgarry@gmail.com>
2020-04-30 19:20:18 -0500

    total makeover

--- page/script.js
+++ page/script.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
+  function submit(){
+     if (document.getElementById("one").value != ""){
+        document.getElementById("divone").innerHTML = document.getElementById("one").value;
+      } else {document.getElementById("divone").innerHTML = "<br>"};
 
-    var lines = 6;
+      if (document.getElementById("two").value != ""){
+         document.getElementById("divtwo").innerHTML = document.getElementById("two").value;
+       } else {document.getElementById("divtwo").innerHTML = "<br>"};
 
-    $('#countMe').keydown(function(e) {
+       if (document.getElementById("three").value != ""){
+          document.getElementById("divthree").innerHTML = document.getElementById("three").value;
+        } else {document.getElementById("divthree").innerHTML = "<br>"};
 
-        newLines = $(this).val().split("\n").length;
-        
-        if(e.keyCode == 13 && newLines >= lines) {
-            return false;
-        }
-    });
-});
+        if (document.getElementById("four").value != ""){
+           document.getElementById("divfour").innerHTML = document.getElementById("four").value;
+         } else {document.getElementById("divfour").innerHTML = "<br>"};
+
+         if (document.getElementById("five").value != ""){
+            document.getElementById("divfive").innerHTML = document.getElementById("five").value;
+          } else {document.getElementById("divfive").innerHTML = "<br>"};
+
+          if (document.getElementById("six").value != ""){
+             document.getElementById("divsix").innerHTML = document.getElementById("six").value;
+           } else {document.getElementById("divsix").innerHTML = "<br>"};
+  }
